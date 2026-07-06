34b8ecc9e11ff87e42071a0ef051bff4fae21db6
humble-coder <mark.philosophe@gmail.com>
2026-07-06 10:35:17 -0400

    Added basic REPL

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,28 @@
+import { createInterface, Interface } from 'readline';
+
 export function cleanInput(input: string): string[] {
-	return input.trim().split(/\s+/);
+	return input.toLowerCase().trim().split(/\s+/);
 }
+
+export let rl: Interface | null = null;
+
+export function startREPL(): void {
+  rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > "
+  });
+
+  rl?.prompt();
+
+  rl.on('line', (line: string) => {
+    const cleanLine = cleanInput(line);
+    if (cleanLine.length === 0 || cleanLine[0] === "") {
+      rl?.prompt();
+    }
+    else {
+      console.log(`Your command was: ${cleanLine[0]}`);
+      rl?.prompt();
+    }
+  });
+};
